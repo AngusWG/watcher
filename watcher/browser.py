ff3c517248f4f3a1a7bb67b0aeca303cf361d2ee
zza <740713651@qq.com>
2019-04-30 14:59:54 +0800

    update

--- watcher/browser.py
+++ watcher/browser.py
@@ -11,7 +11,10 @@ from selenium.webdriver import DesiredCapabilities
 def init_browser(app):
     service_args = ['--ignore-ssl-errors=true', '--ssl-protocol=TLSv1']
     dcap = dict(DesiredCapabilities.PHANTOMJS)
-    dcap["phantomjs.page.settings.userAgent"] = app.config["user_agent"]
+    dcap["phantomjs.page.settings.userAgent"] = "Mozilla/5.0 (Windows NT 10.0; WOW64) " \
+                                                "AppleWebKit/537.36 (KHTML, like Gecko) " \
+                                                "Chrome/63.0.3239.26 Safari/537.36 Core/" \
+                                                "1.63.6716.400 QQBrowser/10.2.2214.40"
     dcap["phantomjs.page.settings.loadImages"] = True
     browser = webdriver.PhantomJS(app.config["phantomjs"],
                                   service_args=service_args,
